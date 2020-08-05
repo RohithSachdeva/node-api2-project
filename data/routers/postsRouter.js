9572999ce5ea708aed73847df193e57da245640a
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-08-05 12:22:48 -0700

    redoing a few CRUD operations

--- data/routers/postsRouter.js
+++ data/routers/postsRouter.js
@@ -1,1 +1,53 @@
 const express = require("express");
+const db = require('../db.js');
+
+const router = express.Router();
+
+//3 get requests; find, findById, findPostComments
+
+router.get("/", (req, res) => {
+    db.find(req.query)
+    .then(posts => res.status(200).json(posts))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({error:"Posts can't be retrieved"})
+    })
+});
+
+router.get("/:id", (req, res) => {
+    db.findById(req.params.id)
+    .then(item => {
+        if (item) {
+            res.status(200).json(item);
+        } else {
+            res.status(404).json({message: "Not found"});
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: "Error retrieving",
+        });
+    });
+});
+
+router.get("/:id/comments", (req, res) => {
+    db.findPostComments(req.params.id)
+    .then(item => {
+        if(item) {
+            res.status(200).json(item)
+        } else {
+            res.status(404).json({ message: "Comment not found"})
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json( { message: "Error"})
+    });
+});
+
+
+//2 Post Requests; Post and comments by id
+
+router.post("/", (req, res))
+module.exports = router;
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require("express");
+const postsRouter = require('./data/routers/postsRouter.js')
 
 const server = express();
+server.use(express.json());
+
+server.use('/api/posts', postsRouter)
+server.listen(8000, () => console.log('Running on port 8000'));
+
